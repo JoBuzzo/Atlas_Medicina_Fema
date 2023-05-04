e79e99517a4c5d251a7e4364edf17606817f3952
João Lucas Buzzo <108816555+JoBuzzo@users.noreply.github.com>
2023-05-04 12:09:30 -0300

    Store Group

---
+++ app/Http/Livewire/Group/Store.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Livewire\Group;
+
+use App\Models\Category;
+use App\Models\Group;
+use Livewire\Component;
+use Livewire\WithFileUploads;
+use Illuminate\Support\Str;
+
+class Store extends Component
+{
+    use WithFileUploads;
+
+    public $categories, $search_category, $selected_categories = [];
+
+    public function mount()
+    {
+        $this->categories = Category::orderBy('name', 'asc')->get();
+    }
+
+    public function render()
+    {
+        $this->categories = Category::where('name', 'LIKE', "%{$this->search_category}%")->orderBy('name', 'asc')->get();
+        return view('livewire.group.store');
+    }
+
+
+
+    public $titleGroup;
+    public $group;
+
+    public function saveGroup()
+    {
+        $this->validate([
+            'titleGroup' => 'required|string|max:60',
+            'selected_categories' => 'required',
+        ]);
+
+        $this->group = Group::create([
+            'title' => $this->titleGroup,
+        ]);
+
+        foreach ($this->selected_categories as $id) {
+            $this->group->categories()->attach($id);
+        }
+    }
+
+
+    public $titleImage;
+    public $image;
+    public $description;
+
+    public function saveImage()
+    {
+        $this->validate([
+            'titleImage' => 'required|string|max:60',
+            'image' => 'image',
+            'description' => 'required|string',
+        ]);
+
+        $nameFile = Str::slug($this->titleImage) . date("Ymdhis") . "." . $this->image->getClientOriginalExtension();
+        $this->image->storeAs('public/images', $nameFile);
+
+        $this->group->images()->create([
+            'image' => $nameFile,
+            'title' => $this->titleImage,
+            'description' => $this->description
+        ]);   
+    }
+
+
+    public function resetImages()
+    {
+        $this->reset('titleImage');
+        $this->reset('image');
+        $this->reset('description');
+    }
+
+
+    public function updated($propertyName)
+    {   
+        $this->validateOnly($propertyName, [
+            'titleGroup' => 'required|string|max:60',
+            'titleImage' => 'required|string|max:60',
+            'image' => 'image',
+            'description' => 'required|string',
+        ]);
+    }
+
+}
---
+++ resources/views/livewire/group/store.blade.php
@@ -0,0 +1,57 @@
+<div>
+    <form wire:submit.prevent='saveGroup'>
+        <input type="text" placeholder="title" wire:model='titleGroup' class="text-black">
+        @error('titleGroup')
+            {{ $message }}
+        @enderror
+
+
+        <ul class="px-3 pb-3 overflow-y-auto text-sm text-gray-700 max-h-48 dark:text-gray-200">
+            @forelse ($categories as $category)
+                <li class="flex items-center p-2 rounded-lg cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-600">
+                    <div class="flex items-center w-full pl-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
+                        <input id="checkbox={{ $category->id }}" type="checkbox" value="{{ $category->id }}"
+                            wire:model="selected_categories"
+                            class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded cursor-pointer focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                        <label for="checkbox={{ $category->id }}"
+                            class="block w-full ml-2 text-sm font-medium text-gray-900 cursor-pointer dark:text-gray-300">
+                            {{ $category->name }}
+                        </label>
+                    </div>
+                </li>
+            @empty
+                <li class="w-full pl-4 mt-2">
+                    <p class="text-sm font-semibold">Não encontrado</p>
+                </li>
+            @endforelse
+        </ul>
+        @error('selected_categories')
+            <span
+                class="block mt-2 text-xs font-medium text-red-600 lg:inline-block dark:text-red-400">{{ $message }}</span>
+        @enderror
+
+        <button>Submit</button>
+    </form>
+
+
+    <button wire:click='resetImages'>Adicionar mais imagens</button>
+    <form wire:submit.prevent='saveImage'>
+
+        <input type="text" wire:model='titleImage' class="text-black">
+        @error('titleImage')
+            {{ $message }}
+        @enderror
+
+        <input type="file" wire:model='image' class="text-black">
+        @error('image')
+            {{ $message }}
+        @enderror
+
+        <input type="text" wire:model='description' class="text-black">
+        @error('description')
+            {{ $message }}
+        @enderror
+
+        <button>Submit</button>
+    </form>
+</div>
