1b9af8b547b526ebcab6866410d880b7948cd844
João Lucas Buzzo <108816555+JoBuzzo@users.noreply.github.com>
2023-05-08 11:10:14 -0300

    merge update img

--- resources/views/livewire/group/show.blade.php
+++ resources/views/livewire/group/show.blade.php
@@ -8,8 +8,8 @@
                         #categoriaVaiAqui
                     </span>
                 </div>
-                <div class="mb-8 lg:mb-12 flex items-center justify-between w-full">
-                    <h1 class="m-0 flex items-center text-5xl font-bold dark:text-white">
+                <div class="flex items-center justify-between w-full mb-8 lg:mb-12">
+                    <h1 class="flex items-center m-0 text-5xl font-bold dark:text-white">
                         {{ $group->title }}
                     </h1>
                     <button type="button"
@@ -19,7 +19,7 @@
                 </div>
 
                 @foreach ($images as $image)
-                    <div class="grid lg:grid-cols-2 w-full gap-6">
+                    <div class="grid w-full gap-6 lg:grid-cols-2">
                         <div class="">
                             <a href="{{ $image->image }}" target="_blank">
                                 <img class="w-full h-full" src="{{ $image->image }}" alt="{{ $image->title }}">
@@ -28,16 +28,13 @@
                         <div class="">
                             <div class="flex items-center justify-between mb-3">
                                 <h2 class="text-2xl font-medium dark:text-white">{{ $image->title }}</h2>
-                                <button
-                                    class="px-3 py-2 text-xs font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                                    editar
-                                </button>
+                                <livewire:components.update-image :id='$image->id' />
                             </div>
                             <p class="">{{ $image->description }}</p>
 
                         </div>
                     </div>
-                    <hr class="my-6 border-gray-300  sm:mx-auto dark:border-gray-700 lg:my-8">
+                    <hr class="my-6 border-gray-300 sm:mx-auto dark:border-gray-700 lg:my-8">
                 @endforeach
 
 
