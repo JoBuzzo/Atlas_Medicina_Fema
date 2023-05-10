f016908174e32f19a09fee0017676bb6c46a7168
João Lucas Buzzo <108816555+JoBuzzo@users.noreply.github.com>
2023-05-10 11:59:30 -0300

    Delete Group

---
+++ app/Http/Livewire/Components/DeleteGroup.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Livewire\Components;
+
+use App\Models\Group;
+use Livewire\Component;
+
+class DeleteGroup extends Component
+{
+    public function render()
+    {
+        return view('livewire.components.delete-group');
+    }
+
+    public $group;
+    public function mount($id)
+    {
+        $this->group = Group::find($id);
+    }
+
+    public $show;
+    public function show()
+    {
+        $this->show = !$this->show;
+    }
+
+
+    public function delete()
+    {
+        $images = $this->group->images;
+
+        foreach ($images as $image){
+            unlink(storage_path('app/public/images/' . $image->getRawOriginal('image')));
+        }
+
+        $this->group->images->delete();
+
+        $this->group->delete();
+
+        session()->flash('msg', 'Grupo deletado com sucesso.');
+        $this->reset('show');
+        return redirect()->route('images.index');
+    }
+
+}
---
+++ resources/views/livewire/components/delete-group.blade.php
@@ -0,0 +1,55 @@
+<div>
+
+    <a wire:click='show' style="color:rgb(239 68 68)"
+        class="flex items-center gap-2 px-4 py-2 text-sm cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5">
+            <path fill-rule="evenodd"
+            d="M16.5 4.478v.227a48.816 48.816 0 013.878.512.75.75 0 11-.256 1.478l-.209-.035-1.005 13.07a3 3 0 01-2.991 2.77H8.084a3 3 0 01-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 01-.256-1.478A48.567 48.567 0 017.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 013.369 0c1.603.051 2.815 1.387 2.815 2.951zm-6.136-1.452a51.196 51.196 0 013.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 00-6 0v-.113c0-.794.609-1.428 1.364-1.452zm-.355 5.945a.75.75 0 10-1.5.058l.347 9a.75.75 0 101.499-.058l-.346-9zm5.48.058a.75.75 0 10-1.498-.058l-.347 9a.75.75 0 001.5.058l.345-9z"
+            clip-rule="evenodd" />
+        </svg>
+        Excluir
+    </a>
+
+    @if ($show)
+        <div
+            class="bg-[#000000bd] overflow-y-auto block fixed top-0 right-0 left-0 z-50 justify-center items-center w-full inset-0 h-modal h-full">
+            <div
+                class="absolute w-full h-full max-w-2xl p-4 transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+                <!-- Modal content -->
+                <div class="relative p-4 text-center bg-white rounded-lg shadow dark:bg-gray-800 sm:p-5">
+                    <button type="button"
+                        class="text-gray-400 absolute top-2.5 right-2.5 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                        wire:click="show">
+                        <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd"
+                                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                clip-rule="evenodd"></path>
+                        </svg>
+                        <span class="sr-only">Fechar modal</span>
+                    </button>
+                    <svg class="text-gray-400 dark:text-gray-500 w-11 h-11 mb-3.5 mx-auto" aria-hidden="true"
+                        fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd"
+                            d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                            clip-rule="evenodd"></path>
+                    </svg>
+                    <p>
+                        Deseja realmente excluir esse grupo?
+                    </p>
+                    <div class="flex items-center justify-center mt-6 space-x-4">
+                        <button type="button" wire:click="show"
+                            class="px-3 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-primary-300 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">
+                            Não, cancelar
+                        </button>
+                        <button wire:click="delete"
+                            class="px-3 py-2 text-sm font-medium text-center text-white bg-red-600 rounded-lg hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-500 dark:hover:bg-red-600 dark:focus:ring-red-900">
+                            Sim, eu tenho certeza
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endif
+</div>
+
